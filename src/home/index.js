46018ba6143f718428c09f9a1b6666e799b8c3f0
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2022-01-31 13:13:47 -0300

    Botões Numéricos

--- src/home/index.js
+++ src/home/index.js
@@ -5,6 +5,20 @@ export default function Home() {
         <div className='body_home'>
             <div className='celular'>
                 <input className='visor' type="text" value="Foodlovers"></input>
+                <div className='buttons'>
+                    <div className='button1'></div>
+                    <div className='button2'></div>
+                    <div className='button3'></div>
+                    <div className='button4'></div>
+                    <div className='button5'></div>
+                    <div className='button6'></div>
+                    <div className='button7'></div>
+                    <div className='button8'></div>
+                    <div className='button9'></div>
+                    <div className='buttonAst'></div>
+                    <div className='button0'></div>
+                    <div className='buttonReg'></div>
+                </div>
             </div>
         </div>
     )
