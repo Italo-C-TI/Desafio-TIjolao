4ddae5f3879c205a4640d88e5741845c06451bc1
italo.costa <italo.costa@itechs-MacBook-Pro.local>
2022-01-31 11:22:42 -0300

    enviando requesicao

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  useState(botao1,setBotao1) = [1]
+  const [numero,setNumero] = useState("");
+  const [ultimoNumeroRecebido,setUltimoNumeroRecebido] = useState();
+  const [codigo,setCodigo] = useState("");
+  const [botao3,setBotao3] = useState("");
+
+  const [maiusculo,setMaiusculo] = useState(false);
+
+  const handleAdicionaAoCodigo =(numeroRecebido)=>{
+    if(numeroRecebido == '1'){
+      return;
+    }
+    setUltimoNumeroRecebido(numeroRecebido)
+    setCodigo(codigo.trim() + numeroRecebido.trim());
+
+  }
+  const handleEnviaCodigo = async () => {
+    try {
+      const response = await fetch(
+        "https://back-modulo-5-teste.herokuapp.com/cobrancas",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            codigo: 1,
+            shift: maiusculo
+          }),
+        }
+      );
+
+
+    } catch (error) {
+
+    }
+  };
   return (
     <div className="App">
-
+        <h2>TESTE</h2>
     </div>
   );
 }
