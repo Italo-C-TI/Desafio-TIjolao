3aafabb4bb527c39762023e3839ba5d2ca08d0ee
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2022-01-31 12:38:22 -0300

    Correção do background

--- src/home/index.js
+++ src/home/index.js
@@ -3,7 +3,7 @@ import './styles.css';
 export default function Home() {
     return (
         <div className='body_home'>
-            <div className='celular'>Teste</div>
+            <div className='celular'></div>
         </div>
     )
 }
